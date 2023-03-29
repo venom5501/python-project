866ea8b2e82c0caa9503f9d4507167dd171d38b0
venom5501 <123529112+venom5501@users.noreply.github.com>
2023-03-30 00:50:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,26 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+  
+
+def delete():
+    print("Enter the word you wanna delete: \n")
+    wordToDelete = input()
+    myDictionary = open("./dictionary.txt", "r")
+    allWords = myDictionary.readlines()
+    flag = True
+    for x in allWords:
+        if f"{wordToDelete}:" in x:
+            allWords.remove(x)
+            dataFile = open('./dictionary.txt',"w")
+            dataFile.writelines(allWords)
+            dataFile.close()
+            print(f"{bcolors.WARNING}Your given word deleted successfully.")
+            flag = False
+            myDictionary.close()
+    if flag:
+        myDictionary.close()
+        print("The word you wanna delete is not found in the dictionary.")
+ 
+
 
